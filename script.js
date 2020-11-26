8d4ed350f5f8d1f6695df3ad49dc4dea08449778
Illiachr <74302872+Illiachr@users.noreply.github.com>
2020-11-26 13:55:17 +0200

    добавил вопрос Сыграем еще

--- script.js
+++ script.js
@@ -9,42 +9,64 @@ const isNumber = ((x) => {
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
-
+// Функция инициализации бота
 const init = (() => {
+// Загадываем число
     const numSecret = getRandomInt(101);
+// Массив ответов игрока
     const userAnswers = [];
+// Флаг правильно ответа TODO --> DELETE
+    let userAnswerIsRight = false;
+// Счетчик кол-ва ответов игрока
     let i = 0;    
 
+// бот
     const botGame = (() => {
-    const botQuestions = (() => {
-        userAnswers[i] = parseInt(userAnswers[i]);
-        userAnswers[i] === numSecret ? alert ('Поздравляю! Ты угадал!') : 
-        userAnswers[i] > numSecret ? alert (` Не угадал мое число меньше, попробуй снова!
-        Твои ответы: ${userAnswers.join(' ')}`) :
-        alert (` Не угадал мое число больше, попробуй снова!
-        Твои ответы: ${userAnswers.join(' ')}`);
-        i++;
-    });
+// Функция проверки ответа
+        const botQuestions = (() => {
+            userAnswers[i] = parseInt(userAnswers[i]);
 
+            if (userAnswers[i] === numSecret) { 
+                alert ('Поздравляю! Ты угадал');
+                    if (confirm ('Давай сыграем ещё?')) {
+                        //i = 0;
+                        botGame(); //Играем снова
+                    } else { // Если игрок нажад "Отмена"
+                        alert('Хорошего дня! С нетерпением жду новых встреч!'); 
+                        } 
+            } else if (userAnswers[i] > numSecret) {
+                alert (` Не угадал мое число меньше, попробуй снова!
+                Твои ответы: ${userAnswers.join(' ')}`);
+                i++;
+                } else {
+                    alert (` Не угадал мое число больше, попробуй снова!
+                    Твои ответы: ${userAnswers.join(' ')}`);
+                    i++;
+                    }       
+        });
+// Функция варианта когда ввели не число
     const notNum = (() => {
         alert ('Нужно вводить только числа, попробуй снова!');
         userAnswers[i] = prompt ('Попробуй угадай :)) Напиши число от 1 до 100', '1');
         if (!isNumber(userAnswers[i])) { 
-            notNum(userAnswers[i]);
+            notNum();
         } else { botQuestions(); }
+    });
+// Получаем ответ игрока    
+    userAnswers[i] = prompt ('Попробуй угадай :)) Напиши число от 1 до 100', '1');
+// Проверяем
+    if (userAnswers[i-1] !== numSecret && userAnswers[i] !== null) {// Если не равно загаданному и нажали "Отмена"
+    if (!isNumber(userAnswers[i])) { // Проверяем является ли ответ числом
+        notNum(); // Если нет просим ввести ответ снова
+    } else { botQuestions(); } // Если да - новая попытка
+
+    botGame(); // Рекусивный вызов botGame()
+    // Если игрок нажад "Отмена"
+    } else if (userAnswers[i] === null) { 
+        alert ('Очень жаль, надеюсь, ты скоро вернешся :.('); 
+        }
     });
     
-    userAnswers[i] = prompt ('Попробуй угадай :)) Напиши число от 1 до 100', '1');
-
-    if (userAnswers[i] !== null) {
-        if (!isNumber(userAnswers[i])) { 
-            notNum(userAnswers[i]);
-        } else { botQuestions(); }
-
-        botGame ();
-    } else return alert (`Твои ответы: ${userAnswers.join(' ')}`);    
-    });
-
     confirm (` Привет! Давай играть в угадайку :) 
     Я загадываю число от 1 до 100, а ты угадываешь, готов?`) ? 
     botGame () :
@@ -56,4 +78,3 @@ const init = (() => {
 });
 
 init();
-console.dir (init);
